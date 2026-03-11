9dca29e1fef3dab32fa380312ac9e757d9384edf
Giulio V <vannini.gv@gmail.com>
2026-03-11 10:04:56 +0100

    chore(cube): remove old Cube Wizard and Definition Editor, update barrel exports

--- rdf-forge/rdf-forge-ui/src/app/features/cube/index.ts
+++ rdf-forge/rdf-forge-ui/src/app/features/cube/index.ts
@@ -1,12 +1,3 @@
 // Cube feature exports
-
-// Components
 export { CubeList } from './cube-list/cube-list';
-export { CubeWizard } from './cube-wizard/cube-wizard';
-export {
-  CubeDefinitionEditor,
-  CubeDefinition,
-  CubeComponent,
-  ColumnMapping
-} from './cube-definition-editor/cube-definition-editor';
 export { CubeProject } from './cube-project/cube-project';
